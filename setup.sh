5b269fc52d9903709aa952f0db249230c380c5d3
ManuelReschke <manuel.reschke90@gmail.com>
2024-07-26 15:36:00 +0200

    improvements

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt update
 apt upgrade -y
 
 # install make to execute makefile commands
-apt install -y make
+apt install -y make unzip
 
 # install docker and docker-compose
 curl -o docker.sh https://raw.githubusercontent.com/ManuelReschke/linux-helper/master/ubuntu/docker/install-update.sh
@@ -13,11 +13,17 @@ chmod +x docker.sh
 sudo ./docker.sh
 
 # download & unzip repo files
-curl -o caddy-reverse-proxy.zip https://github.com/ManuelReschke/caddy-reverse-proxy/archive/refs/heads/main.zip
+curl -L -o caddy-reverse-proxy.zip https://github.com/ManuelReschke/caddy-reverse-proxy/archive/refs/heads/main.zip
 mkdir reverse
-unzip caddy-reverse-proxy.zip -d reverse
+unzip caddy-reverse-proxy.zip
+mv caddy-reverse-proxy-main/* reverse/
+rm -r caddy-reverse-proxy-main/
 
 # modify
+echo "
+######################################
+Successfully prepared!
+######################################"
 echo "Modify your Caddyfile.prod.dist file!"
 echo "Start your local reverse proxy with the command: make start"
 echo "Start your prod reverse proxy with the command: make prod-start"
